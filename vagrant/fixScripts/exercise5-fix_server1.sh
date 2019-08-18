7cc5ec1a41885c8afe5a9db40a1bbb5dd8453b2a
dordagani <dordagani1@gmail.com>
2019-08-18 16:04:41 +0300

    finalgit push origin winter2019!

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get install sshpass -y
-sudo -H -u vagrant bash -c 'echo -e "\n" | ssh-keygen -N "" &> /dev/null'
+sudo -H -u vagrant bash -c 'echo -e "\n" | ssh-keygen -N "" &> /dev/null' #for vagrant user
+echo -e "\n" | ssh-keygen -N "" &> /dev/null #for root user
 sudo sed -i '1s/^/Host *\n   StrictHostKeyChecking no\n   UserKnownHostsFile=\/dev\/null\n/' /etc/ssh/ssh_config
-#sudo ping -c 3 192.168.100.11 > /tmp/ping.txt > this command will run from server2 to server1 later, because server2 is non available when server1 is set up
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get install sshpass -y
-sudo -H -u vagrant bash -c 'echo -e "\n" | ssh-keygen -N "" &> /dev/null'
+sudo -H -u vagrant bash -c 'echo -e "\n" | ssh-keygen -N "" &> /dev/null' #For vagrant user
+echo -e "\n" | ssh-keygen -N "" &> /dev/null #For root user
 sudo sed -i '1s/^/Host *\n   StrictHostKeyChecking no\n   UserKnownHostsFile=\/dev\/null\n/' /etc/ssh/ssh_config
-sudo -H -u vagrant bash -c 'sshpass -p "vagrant" ssh-copy-id vagrant@192.168.100.10'
-sudo -H -u vagrant bash -c "ssh vagrant@192.168.100.10 'sshpass -p "vagrant" ssh-copy-id vagrant@192.168.100.11'"
+sudo -H -u vagrant bash -c 'sshpass -p "vagrant" ssh-copy-id vagrant@192.168.100.10' #for vagrant user 
+sshpass -p "vagrant" ssh-copy-id vagrant@192.168.100.10 #for root user
+sudo -H -u vagrant bash -c "ssh vagrant@192.168.100.10 'sshpass -p "vagrant" ssh-copy-id vagrant@192.168.100.11'" #for vagrant user on server1
+ssh vagrant@192.168.100.10 "sudo su -c 'sshpass -p "vagrant" ssh-copy-id vagrant@192.168.100.11'" #for root user on server1
